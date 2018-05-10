c4edf824732c214c0a59269995c28e8a9c655c10
ben <2388697970@qq.com>
2018-05-11 00:43:53 +0800

    feat: add a simp template

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const Koa = require('koa')
 const app = new Koa()
+const tpl = require('./tpl/normal')
+
 app.use(async (ctx, next) => {
-  ctx.body = 'hello ben'
+  ctx.type = 'text/html; chartset=utf-8'
+  ctx.body = tpl
 })
 app.listen(8080, console.log.bind(null, 'running in 8080...'))
---
+++ server/tpl/normal.js
@@ -0,0 +1,26 @@
+module.exports = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>Home</title>
+  <link href="https://cdn.bootcss.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdn.bootcss.com/bootstrap/4.1.0/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+  <div class="container">
+  <div class="row">
+    <div class="col-md-8">
+      <h1>Hi Tom</h1>
+      <p>This is a server page</p>
+    </div>
+    <div class="col-md-4">
+      <p>Welcome to this page!</p>
+    </div>
+  </div>
+  </div>
+</body>
+</html>
+`
