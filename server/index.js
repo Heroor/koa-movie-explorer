af65c771614cc1c215bdfaea5aea2cb5e30298b7
ben <2388697970@qq.com>
2018-05-13 12:40:46 +0800

    增加模板引擎中间件 分解pug模板

--- server/index.js
+++ server/index.js
@@ -1,22 +1,16 @@
+const { resolve } = require('path')
 const Koa = require('koa')
 const app = new Koa()
-const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
-const ejs = require('ejs')
-const pug = require('pug')
+const views = require('koa-views')
+
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 
 app.use(async(ctx, next) => {
-  ctx.type = 'text/html; chartset=utf-8'
-  // html
-  // ctx.body = htmlTpl
-
-  // ejs
-  // ctx.body = ejs.render(ejsTpl, {
-  //   name: 'Tom2'
-  // })
-
-  // pug
-  ctx.body = pug.render(pugTpl, {
+  await ctx.render('index', {
     name: 'Tom'
   })
 })
+
 app.listen(8080, console.log.bind(null, 'running in 8080...'))
