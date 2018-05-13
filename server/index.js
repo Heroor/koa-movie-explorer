89f27646ee1e63e2f7f5894a7c0ed89aa7603e9e
ben <2388697970@qq.com>
2018-05-13 18:26:34 +0800

    配置babel

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const { resolve } = require('path')
 const Koa = require('koa')
 const app = new Koa()
 const views = require('koa-views')
+const static = require('koa-static')
+
+app.use(static(resolve(__dirname, './public')))
 
 app.use(views(resolve(__dirname, './views'), {
   extension: 'pug'
