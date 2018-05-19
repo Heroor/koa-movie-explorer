1129a989a27df01f51b0f32416dfa7f8d67675da
Bener <2388697970@qq.com>
2018-05-19 14:53:23 +0800

    添加注释

--- server/crawler/trailer-list.js
+++ server/crawler/trailer-list.js
@@ -6,29 +6,30 @@ const sleep = time => new Promise(resolve => {
   setTimeout(resolve, time)
 })
 
-;
-(async() => {
+;(async() => {
   console.log('start visit the target page')
   try {
+    // 启动无头浏览器
     const browser = await puppeteer.launch({
       args: ['--no-sandbox'], // 非沙箱模式
       dumpio: false
     })
-    const page = await browser.newPage() // 开启新页面
-    // 打开网站
+    const page = await browser.newPage() // 打开新页面
+    // 打开网页
     await page.goto(url, {
       waitUntil: 'networkidle2'
     })
     await sleep(3000)
-
     // 等待'加载更多'按钮出现
     await page.waitForSelector('.more')
 
+    // 多次点击加载更多
     for (let i = 0; i < 2; i++) {
       await sleep(3000)
       // 点击加载更多按钮
       await page.click('.more')
     }
+    // 获取页面数据
     const result = await page.evaluate(() => {
       var $ = window.$
       var items = $('.list-wp a')
@@ -52,7 +53,9 @@ const sleep = time => new Promise(resolve => {
     })
     // 关闭浏览器
     browser.close()
+    // 子进程传输数据
     process.send({ result })
+    // 退出子进程
     process.exit(0)
   } catch (e) {
     console.log('error==>', e)
